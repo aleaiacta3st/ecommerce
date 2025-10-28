d45b526061eaa57573b698aecc93562197f493a8
aleaiacta3st <invicinc@gmail.com>
2025-10-28 04:05:06 -0400

    Add debug print to prod.py

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -2,10 +2,12 @@ import os
 import dj_database_url
 from .common import *
 
+print("=" * 50)
+print("LOADING PROD.PY!")
+print("=" * 50)
+
 DEBUG = False
-
-SECRET_KEY = os.environ.get('SECRET_KEY', 'temporary-key-for-debug')
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'temp-key')
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-842d5.up.railway.app']
 
