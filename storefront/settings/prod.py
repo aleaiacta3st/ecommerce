78c14fde67375ba0ab610cbc2b40c2841058af5b
aleaiacta3st <invicinc@gmail.com>
2025-10-28 04:14:06 -0400

    FIX: Set ALLOWED_HOSTS properly

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -2,14 +2,12 @@ import os
 import dj_database_url
 from .common import *
 
-print("=" * 50)
-print("LOADING PROD.PY!")
-print("=" * 50)
-
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY', 'temp-key')
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['web-production-842d5.up.railway.app', '*']
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-842d5.up.railway.app']
 
 DATABASES = {
     'default': dj_database_url.config()
